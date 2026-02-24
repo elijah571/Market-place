a7da4ac99fb08aa42cdda52cb847180719323183
elijah571 <elijahfx43@gmail.com>
2026-02-24 16:03:27 +0100

    search functionality enhance

---
+++ Frontend/src/components/NoProduct.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NoProduct = ({ keyword }) => {
+  return (
+    <div className="no-product-content">
+      <div className="no-products-icon">
+        <h3>No Products Found</h3>
+        🧺
+        <p className="no-product-message">
+          {keyword
+            ? `We could't  find any products mathcing "${keyword}" Try using different keywords or browswe our complte catalog`
+            : 'No product available please check back later'}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NoProduct;
--- Frontend/src/pages/Products.jsx
+++ Frontend/src/pages/Products.jsx
@@ -9,6 +9,7 @@ import { getProduct, removeErrors } from '../features/products/productSlice';
 import { toast } from 'react-toastify';
 import Loader from '../components/Loader';
 import { useLocation } from 'react-router-dom';
+import NoProduct from '../components/NoProduct';
 
 const Products = () => {
   const { loading, error, products } = useSelector((state) => state.product);
@@ -43,11 +44,15 @@ const Products = () => {
           {/* Render Categories */}
         </div>
         <div className="products-section">
-          <div className="products-product-container">
-            {products?.map((product) => (
-              <Product key={product?._id} product={product} />
-            ))}
-          </div>
+          {products.length > 0 ? (
+            <div className="products-product-container">
+              {products.map((product) => (
+                <Product key={product._id} product={product} />
+              ))}
+            </div>
+          ) : (
+            <NoProduct />
+          )}
         </div>
       </div>
       <Footer />
